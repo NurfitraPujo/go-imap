7ce6800fe87724ae017b427edada2d50d175c9bb
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2025-07-15 19:16:21 +0700

    feat(fetch): add gmail imap extension header when fetching emails

--- imapclient/fetch.go
+++ imapclient/fetch.go
@@ -57,6 +57,9 @@ func writeFetchItems(enc *imapwire.Encoder, numKind imapwire.NumKind, options *i
 		"INTERNALDATE":  options.InternalDate,
 		"RFC822.SIZE":   options.RFC822Size,
 		"MODSEQ":        options.ModSeq,
+		"X-GM-THRID":    true,
+		"X-GM-MSGID":    true,
+		"X-GM-LABELS":   true,
 	}
 	for k, req := range m {
 		if req {
